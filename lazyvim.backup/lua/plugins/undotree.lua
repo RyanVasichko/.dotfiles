d0f4f6f8289b51fd533cf70c6faea9badc9dd1ca
Ryan Vasichko <RyanVasichko@gmail.com>
2025-09-07 09:07:36 -0500

    removed lazyvim backup

--- lazyvim.backup/lua/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mbbill/undotree",
-  keys = {
-    { "<leader>ut", vim.cmd.UndotreeToggle, desc = "Toggle UndoTree" },
-  },
-  config = function()
-    vim.g.undotree_WindowLayout = 2
-  end,
-}
--- lazyvim.backup/lua/plugins/which-key.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "folke/which-key.nvim",
-  opts = {
-    -- height = { min = 10, max = 25 },
-  },
-}
